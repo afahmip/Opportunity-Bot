3912754361d9c76e3b90ad7d1e5b70da70315321
ahmadfahmip <afahmi3@yahoo.co.id>
2018-08-14 22:45:49 +0700

    Fix bug on flex message type

--- src/utils/msg_object_handler.js
+++ src/utils/msg_object_handler.js
@@ -17,67 +17,71 @@ function imageSendMessage(originalContentUrl, previewImageUrl) {
 
 function carouselSendMessage() {
     return {
-        "type": "carousel",
-        "contents": [
-        {
-            "type": "bubble",
-            "body": {
-            "type": "box",
-            "layout": "horizontal",
+        "type": "flex",
+        "altText": "Halo semua",
+        "contents": {
+            "type": "carousel",
             "contents": [
                 {
-                "type": "text",
-                "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                "wrap": true
-                }
-            ]
-            },
-            "footer": {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
+                    "type": "bubble",
+                    "body": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                        "type": "text",
+                        "text": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                        "wrap": true
+                        }
+                    ]
+                    },
+                    "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                        "type": "button",
+                        "style": "primary",
+                        "action": {
+                            "type": "uri",
+                            "label": "Go",
+                            "uri": "https://example.com"
+                        }
+                        }
+                    ]
+                    }
+                },
                 {
-                "type": "button",
-                "style": "primary",
-                "action": {
-                    "type": "uri",
-                    "label": "Go",
-                    "uri": "https://example.com"
-                }
-                }
-            ]
-            }
-        },
-        {
-            "type": "bubble",
-            "body": {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-                {
-                "type": "text",
-                "text": "Hello, World!",
-                "wrap": true
-                }
-            ]
-            },
-            "footer": {
-            "type": "box",
-            "layout": "horizontal",
-            "contents": [
-                {
-                "type": "button",
-                "style": "primary",
-                "action": {
-                    "type": "uri",
-                    "label": "Go",
-                    "uri": "https://example.com"
-                }
+                    "type": "bubble",
+                    "body": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                        "type": "text",
+                        "text": "Hello, World!",
+                        "wrap": true
+                        }
+                    ]
+                    },
+                    "footer": {
+                    "type": "box",
+                    "layout": "horizontal",
+                    "contents": [
+                        {
+                        "type": "button",
+                        "style": "primary",
+                        "action": {
+                            "type": "uri",
+                            "label": "Go",
+                            "uri": "https://example.com"
+                        }
+                        }
+                    ]
+                    }
                 }
             ]
-            }
         }
-        ]
     }
 }
 
@@ -111,76 +115,79 @@ function flexSendMessage(header, textMessages) {
     //     }
     // }
 
-    /*
+    
     return {
-        "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                textSendMessage(header)
-            ]
-        },
-        "hero": {
-            "type": "image",
-            "url": "https://files.catbox.moe/nkc088.png",
-            "size": "full",
-            "aspectRatio": "2:1"
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": textMessages
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                textSendMessage("Footer")
-            ]
+        "type": "flex",
+        "altText": "Halo semua",
+        "contents": {
+            "type": "bubble",
+            "header": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    textSendMessage(header)
+                ]
+            },
+            "hero": {
+                "type": "image",
+                "url": "https://files.catbox.moe/nkc088.png",
+                "size": "full",
+                "aspectRatio": "2:1"
+            },
+            "body": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": textMessages
+            },
+            "footer": {
+                "type": "box",
+                "layout": "vertical",
+                "contents": [
+                    textSendMessage("Footer")
+                ]
+            }
         }
     }
-    */
 
-    return {
-        "type": "bubble",
-        "header": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "header"
-                }
-            ]
-        },
-        "hero": {
-            "type": "image",
-            "url": "https://files.catbox.moe/nkc088.png",
-            "size": "full",
-            "aspectRatio": "2:1"
-        },
-        "body": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "body"
-                }
-            ]
-        },
-        "footer": {
-            "type": "box",
-            "layout": "vertical",
-            "contents": [
-                {
-                    "type": "text",
-                    "text": "footer"
-                }
-            ]
-        }
-    }
+    // return {
+    //     "type": "bubble",
+    //     "header": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": [
+    //             {
+    //                 "type": "text",
+    //                 "text": "header"
+    //             }
+    //         ]
+    //     },
+    //     "hero": {
+    //         "type": "image",
+    //         "url": "https://files.catbox.moe/nkc088.png",
+    //         "size": "full",
+    //         "aspectRatio": "2:1"
+    //     },
+    //     "body": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": [
+    //             {
+    //                 "type": "text",
+    //                 "text": "body"
+    //             }
+    //         ]
+    //     },
+    //     "footer": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": [
+    //             {
+    //                 "type": "text",
+    //                 "text": "footer"
+    //             }
+    //         ]
+    //     }
+    // }
 }
 
 module.exports = {
