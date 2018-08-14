dfce8835c43bad346b90f5179b4cbc7a41086040
ahmadfahmip <afahmi3@yahoo.co.id>
2018-08-14 22:36:04 +0700

    Fix bug on flexmessage

--- src/utils/msg_object_handler.js
+++ src/utils/msg_object_handler.js
@@ -16,7 +16,37 @@ function imageSendMessage(originalContentUrl, previewImageUrl) {
 }
 
 function flexSendMessage(header, textMessages) {
-    return {  
+    // return {  
+    //     "type": "bubble",
+    //     "header": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": [
+    //             textSendMessage(header)
+    //         ]
+    //     },
+    //     "hero": {
+    //         "type": "image",
+    //         "url": "https://files.catbox.moe/nkc088.png",
+    //         "size": "full",
+    //         "aspectRatio": "2:1"
+    //     },
+    //     "body": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": textMessages
+    //     },
+    //     "footer": {
+    //         "type": "box",
+    //         "layout": "vertical",
+    //         "contents": [
+    //             textSendMessage("Footer")
+    //         ]
+    //     }
+    // }
+
+    /*
+    return {
         "type": "bubble",
         "header": {
             "type": "box",
@@ -41,6 +71,47 @@ function flexSendMessage(header, textMessages) {
             "layout": "vertical",
             "contents": [
                 textSendMessage("Footer")
+            ]
+        }
+    }
+    */
+
+    return {
+        "type": "bubble",
+        "header": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": "header"
+                }
+            ]
+        },
+        "hero": {
+            "type": "image",
+            "url": "https://example.com/flex/images/image.jpg",
+            "size": "full",
+            "aspectRatio": "2:1"
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": "body"
+                }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": "footer"
+                }
             ]
         }
     }
