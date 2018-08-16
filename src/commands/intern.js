e8c5953d21198765a77000b867879f72b7662fe6
ahmadfahmip <afahmi3@yahoo.co.id>
2018-08-16 08:30:25 +0700

    Add spacing to buuble intern.js

--- src/commands/intern.js
+++ src/commands/intern.js
@@ -25,39 +25,12 @@ function showRole(role) {
         // "header": msg.createComponent("box", "vertical", [msg.textSendMessage("Roles Available")]),
         let message = {
             "type": "bubble",
+            "header": msg.createComponent("box", "vertical", [msg.textSendMessage("Roles Available")]),
             "body": {
                 "type": "box",
                 "layout": "vertical",
                 "spacing": "md",
-                "contents": [
-                    {
-                        "type": "button",
-                        "style": "primary",
-                        "action": {
-                            "type": "uri",
-                            "label": "Primary style button",
-                            "uri": "https://example.com"
-                        }
-                    },
-                    {
-                        "type": "button",
-                        "style": "secondary",
-                        "action": {
-                            "type": "uri",
-                            "label": "Secondary style button",
-                            "uri": "https://example.com"
-                        }
-                    },
-                    {
-                        "type": "button",
-                        "style": "link",
-                        "action": {
-                            "type": "uri",
-                            "label": "Link style button",
-                            "uri": "https://example.com"
-                        }
-                    }
-                ]
+                "contents": textMessages
             }
         }
         return msg.flexSendMessage("[ROLES AVAILABLE]", message);
